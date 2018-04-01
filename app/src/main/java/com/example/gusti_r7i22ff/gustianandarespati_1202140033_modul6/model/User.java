938d15bccbaaa9b095f066fe66069b6e722a3f15
gustinndrspt@gmail.com <gustinndrspt@gmail.com>
2018-04-01 23:48:50 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/gusti_r7i22ff/gustianandarespati_1202140033_modul6/model/User.java
@@ -0,0 +1,29 @@
+package com.example.gusti_r7i22ff.gustianandarespati_1202140033_modul6.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
